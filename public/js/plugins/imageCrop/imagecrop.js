b090dfd765c12190aec8fe87d3ba6ae69f25ffe8
Mitesh Shah <mitesh24shah@gmail.com>
2014-08-23 16:59:13 +0530

    newly added

---
+++ public/js/plugins/imageCrop/imagecrop.js
@@ -0,0 +1,121 @@
+(function(window, $){
+  var imageCrop = function($image, options){
+      this.$image = $($image);
+      this.options = options;
+	  this.$outline = "";
+    };
+
+  imageCrop.prototype = {
+    defaults: {
+     minSelect : [50, 50],
+	 outlines : [],
+     count:0
+    },
+    init: function() {
+			this.config = $.extend({}, this.defaults, this.options);
+			var $holder = $('<div id="img-container" />')
+					.css({
+						position : 'relative'
+					})
+					.width(this.$image.width())
+					.height(this.$image.height());
+
+			this.$image.wrap($holder)
+				.css({
+					position : 'absolute'
+				});
+			$this = this;
+			this.$image.mousedown(function(event){$this.setSelection($this,event)});
+			return this;
+    },
+	getElementOffset: function(object) {
+		var offset = $(object).offset();
+
+		return [offset.left, offset.top];
+	},
+	getMousePosition: function(event) {
+		var imageOffset = this.getElementOffset(this.$image);
+
+		var x = event.pageX - imageOffset[0],
+			y = event.pageY - imageOffset[1];
+
+		x = (x < 0) ? 0 : (x > this.$image.width()) ? this.$image.width() : x;
+		y = (y < 0) ? 0 : (y > this.$image.height()) ? this.$image.height() : y;
+
+		return [x, y];
+	},
+	addNewSelection: function() {
+		$this = this;
+		var id = this.config.count++;
+		$(".image-crop-outline").css({borderColor:'#ffffff'});
+		this.$outline = $('<div class="image-crop-outline" id="outline'+"-"+id+'"/>')
+			.css({
+				opacity : this.config.outlineOpacity,
+				position : 'absolute',
+				borderColor:'#99C8FF',
+				width: this.config.minSelect[0],
+				height: this.config.minSelect[1]
+			})
+			.insertAfter(this.$image);
+
+		this.config.outlines[id]=this.$outline;
+		//console.log($image);
+		this.$outline.resizable({containment:"#img-container"}).draggable({containment:"#img-container"});
+		this.$outline.on('click',function(){
+			var cid = $this.$outline.attr('id').replace('outline-',"");
+			var nid = $(this).attr('id').replace('outline-',"");
+			if (cid != nid)				
+			{
+				$(".image-crop-outline").css({borderColor:'#ffffff'});
+				$this.$outline = $this.config.outlines[nid];
+				$this.$outline.css({borderColor:'#99C8FF'});
+			}
+		});
+		this.$outline.on('dblclick',function(){
+			var nid = $(this).attr('id').replace('outline-',"");
+			delete $this.config.outlines[nid];
+			$(this).remove();
+		});
+	},
+	setSelection: function($this,event) {
+		$this.addNewSelection();
+		console.log($this);
+		event.preventDefault();
+		event.stopPropagation();
+
+		selectionOrigin = $this.getMousePosition(event);
+		$this.$outline.css({
+				cursor : 'all-scroll',
+				display : 'block',
+				left : selectionOrigin[0],
+				top : selectionOrigin[1]
+			})
+	},
+	getSelection: function() {
+		var $pos = [];
+		$.each(this.config.outlines,function(e,v){
+			if (typeof(v) != "undefined")
+			{
+				$pos.push({x:$(v).css("left") ,y:$(v).css("top") ,w:$(v).width() ,h:$(v).height() });
+			}
+		})
+		return $pos;
+	}
+	 
+  }
+
+  imageCrop.defaults = imageCrop.prototype.defaults;
+
+  $.fn.imageCrop = function(options) {
+	return this.each(function() {
+			var currentObject = this,
+			image = new Image();
+			image.onload = function() {
+				currentObject.crop = new imageCrop(currentObject, options).init();
+			};
+			image.src = currentObject.src;
+		});
+	  };
+
+  window.imageCrop = imageCrop;
+})(window, jQuery);
