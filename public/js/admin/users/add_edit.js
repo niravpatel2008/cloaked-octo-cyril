7294da33d48c4ec23c80b050dad0575396d5d894
Mitesh Shah <mitesh24shah@gmail.com>
2014-09-11 18:38:12 +0530

    modified js

--- public/js/admin/users/add_edit.js
+++ public/js/admin/users/add_edit.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$("#u_birthdate").daterangepicker({singleDatePicker:true,format: 'MM/DD/YYYY'});
+	$("#u_birthdate").daterangepicker({singleDatePicker:true,format: 'MM/DD/YYYY',startDate: '01/01/1901'});
 	$("#user_form").validationEngine();
 });
